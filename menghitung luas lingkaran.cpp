7b96f86c69e23555997a1e1c1ada195518184f41
cici cemong <45526554+cicicemong@users.noreply.github.com>
2019-01-06 11:17:03 +0700

    Add files via upload

---
+++ menghitung luas lingkaran.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    float Luas,phi=3.14;
+    cout<<"MENGHITUNG LUAS LINGKARAN"<<endl;
+    cout<<"jari-jari:";cin>>r;
+    Luas=phi*r*r;
+    cout<<"Luas:"<<Luas<<endl;
+    return 0;
+}
